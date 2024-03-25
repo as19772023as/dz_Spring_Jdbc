5dbe0cd30876639ed57ea0c7a170857c1cb1d098
Andrey <as19772023as@gmail.com>
2024-03-25 13:52:36 +0300

    add schema dao

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
-insert into netology_liquibase.customers (name, surname, age, phone_number)
+insert into netology_dao.customers (name, surname, age, phone_number)
 values('Bob', 'Petrov', 30, '8-922-444-67-56'),
       ('Pop', 'Bobrov', 20, '8-922-333-67-56'),
       ('Oly', 'Petrova', 19, '8-922-222-67-56'),
       ('Any', 'Sidorova', 25, '8-922-111-67-56');
 
-insert into netology_liquibase.orders  (order_date, product_name, customer_id, amount)
+insert into netology_dao.orders  (order_date, product_name, customer_id, amount)
 values('2024-01-03', 'Tv', 6,  25000),
       ('2024-01-25', 'DVD', 5,  30000),
       ('2024-02-19', 'DVD-R', 1,  15000),
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
-create schema if not exists netology_liquibase;
+create schema if not exists netology_dao;
 
-create table netology_liquibase.customers
+create table netology_dao.customers
 (
 id bigserial primary key not null,
 name varchar(50) not null,
@@ -9,13 +9,13 @@ age int check(age > 0 and age < 150) not null,
 phone_number varchar(30)
 );
 
-create table netology_liquibase.orders
+create table netology_dao.orders
 (
 order_id bigserial primary key not null,
 order_date date not null,
 product_name varchar(255) not null,
 amount int check(amount > 0 ) not null,
 customer_id integer NOT NULL,
-FOREIGN KEY (customer_id) references netology_liquibase.customers (id)
+FOREIGN KEY (customer_id) references netology_dao.customers (id)
 );
 
